e81e635b2fdd2d361ad25f0f7ff65347b6dfb896
wu kai <wukai2048@gmail.com>
2021-05-18 10:19:23 +0800

    fix: 正确输出 version

--- index.js
+++ index.js
@@ -9,60 +9,62 @@ const handlebars = require('handlebars');
 const symbols = require('log-symbols');
 const download = require('download-git-repo');
 
-program
-  .version('0.1.0', '-V, --version')
-  .command('init <name>')
-  .action((name) => {
-    if (!fs.existsSync(name)) {
-      inquirer
-        .prompt([
-          {
-            name: 'description',
-            message: '请输入项目描述',
-          },
-          {
-            name: 'author',
-            message: '请输入作者名称',
-          },
-        ])
-        .then((answers) => {
-          const spinner = ora('正在下载模板...');
-          spinner.start();
+const packageJson = require('./package.json');
+const version = packageJson.version;
 
-          download(
-            'github:noiron/simple-template#master',
-            name,
-            { clone: false },
-            (err) => {
-              if (err) {
-                spinner.fail();
-                console.log(symbols.error, chalk.red(err));
-              } else {
-                spinner.succeed();
+program.version(version, '-v, --version');
 
-                const meta = {
-                  name: path.basename(name),
-                  description: answers.description,
-                  author: answers.author,
-                };
+program.command('init <name>').action((name) => {
+  if (!fs.existsSync(name)) {
+    inquirer
+      .prompt([
+        {
+          name: 'description',
+          message: '请输入项目描述',
+        },
+        {
+          name: 'author',
+          message: '请输入作者名称',
+        },
+      ])
+      .then((answers) => {
+        const spinner = ora('正在下载模板...');
+        spinner.start();
 
-                const filename = `${name}/package.json`;
+        download(
+          'github:noiron/simple-template#master',
+          name,
+          { clone: false },
+          (err) => {
+            if (err) {
+              spinner.fail();
+              console.log(symbols.error, chalk.red(err));
+            } else {
+              spinner.succeed();
 
-                // 修改 package.json 中的内容
-                if (fs.existsSync(filename)) {
-                  const content = fs.readFileSync(filename).toString();
-                  const result = handlebars.compile(content)(meta);
-                  fs.writeFileSync(filename, result);
-                }
+              const meta = {
+                name: path.basename(name),
+                description: answers.description,
+                author: answers.author,
+              };
 
-                console.log(symbols.success, chalk.green('项目初始化完成'));
+              const filename = `${name}/package.json`;
+
+              // 修改 package.json 中的内容
+              if (fs.existsSync(filename)) {
+                const content = fs.readFileSync(filename).toString();
+                const result = handlebars.compile(content)(meta);
+                fs.writeFileSync(filename, result);
               }
+
+              console.log(symbols.success, chalk.green('项目初始化完成'));
             }
-          );
-        });
-    } else {
-      console.log(symbols.error, chalk.red('项目已存在'));
-    }
-  });
+          }
+        );
+      });
+  } else {
+    console.log(symbols.error, chalk.red('项目已存在'));
+  }
+});
 
 program.parse(process.argv);
